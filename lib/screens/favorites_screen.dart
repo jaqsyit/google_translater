5309849998bfc938be3406df7f4208b0989c8209
jaqsyit <107976983+jaqsyit@users.noreply.github.com>
2023-07-24 20:02:07 +0500

    Add comments

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,3 +1,27 @@
+/*
+  FavoritesScreen - Экран отображения избранных переводов
+
+  Этот экран отображает список избранных переводов, сохраненных в Hive, с использованием
+  пакета hive и hive_flutter для работы с базой данных локального хранилища.
+
+  Импорт пакетов:
+  - flutter/material.dart: Библиотека для создания пользовательского интерфейса.
+  - hive/hive.dart: Пакет для работы с Hive - легковесной базой данных локального хранилища.
+  - hive_flutter/hive_flutter.dart: Пакет для поддержки Hive в Flutter приложениях.
+  - translater/screens/widgets/favorites_util.dart: Импорт класса Favorites, определенного в другом файле.
+
+  Класс FavoritesScreen - StatelessWidget, который отображает список избранных переводов.
+
+  Метод build:
+  - Возвращает Scaffold, который содержит AppBar с заголовком "Избранные переводы" и тело с виджетом ValueListenableBuilder.
+  - В ValueListenableBuilder происходит прослушивание изменений в Hive Box с избранными переводами.
+  - При изменении данных в Box, строится список ListTile, отображающий каждый избранный перевод.
+
+  Замечания:
+  - Для правильной работы экрана, убедитесь, что у вас настроены и работают пакеты hive и hive_flutter.
+  - Для сохранения переводов в избранное, используйте утилиту FavoritesUtil или схожие методы.
+*/
+
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -8,24 +32,35 @@ class FavoritesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Избранные переводы'),
+        title: Text('Избранные переводы'), // Заголовок AppBar
       ),
       body: ValueListenableBuilder<Box<Favorites>>(
-        valueListenable: Hive.box<Favorites>('Favorites').listenable(),
+        valueListenable: Hive.box<Favorites>('Favorites')
+            .listenable(), // Прослушивание изменений в Hive Box с избранными переводами
         builder: (context, box, _) {
-          final favoritesList = box.values.toList().cast<Favorites>();
+          final favoritesList = box.values
+              .toList()
+              .cast<Favorites>(); // Преобразование избранных переводов в список
           if (favoritesList.isEmpty) {
+            // Если список избранных пуст
             return Center(
-              child: Text('Список избранных пуст'),
+              child: Text(
+                  'Список избранных пуст'), // Вывод сообщения о пустом списке
             );
           } else {
+            // Если список избранных не пуст
             return ListView.builder(
-              itemCount: favoritesList.length,
+              itemCount: favoritesList
+                  .length, // Количество элементов в списке избранных переводов
               itemBuilder: (context, index) {
-                final favorite = favoritesList[index];
+                final favorite = favoritesList[
+                    index]; // Получение избранного перевода по индексу
                 return ListTile(
-                  title: Text(favorite.originalText),
-                  subtitle: Text(favorite.translatedText),
+                  // Вывод ListTile для каждого избранного перевода
+                  title: Text(favorite
+                      .originalText), // Отображение оригинального текста
+                  subtitle: Text(favorite
+                      .translatedText), // Отображение переведенного текста
                 );
               },
             );
--- lib/screens/widgets/favorites_util.dart
+++ lib/screens/widgets/favorites_util.dart
@@ -1,7 +1,28 @@
+/* favorites_util.dart
+/
+/ Этот файл содержит определение класса Favorites, который используется
+/ для представления избранных переводов, и функции saveToFavorites, которая
+/ служит для сохранения перевода в избранные.
+/
+/ Favorites - класс для представления избранного перевода. Он использует Hive
+/ аннотации (@HiveType и @HiveField) для сериализации и десериализации объектов
+/ этого класса в Hive Box.
+/
+/ saveToFavorites - функция для сохранения перевода в избранные. Она принимает два
+/ аргумента: originalText (текст оригинала) и translatedText (переведенный текст).
+/ Функция проверяет, есть ли уже запись с такими же originalText и translatedText
+/ в Hive Box 'Favorites'. Если запись не существует, она создает новый объект Favorites
+/ с уникальным идентификатором и сохраняет его в Hive Box.
+*/
+
 import 'package:hive/hive.dart';
 
+// Генерируемый код для сериализации/десериализации объектов Favorites
 part 'favorites_util.g.dart';
 
+/// Класс Favorites - представление избранного перевода.
+/// Использует аннотации Hive (@HiveType и @HiveField) для сериализации и десериализации
+/// объектов этого класса в Hive Box.
 @HiveType(typeId: 0)
 class Favorites {
   @HiveField(0)
@@ -13,22 +34,37 @@ class Favorites {
   @HiveField(2)
   final String translatedText;
 
+  /// Конструктор класса Favorites
   Favorites(this.id, this.originalText, this.translatedText);
 }
 
+/* Функция saveToFavorites - сохраняет перевод в избранные.
+/ Принимает два аргумента: originalText (текст оригинала) и translatedText (переведенный текст).
+/ Функция проверяет, есть ли уже запись с такими же originalText и translatedText
+/ в Hive Box 'Favorites'. Если запись не существует, она создает новый объект Favorites
+/ с уникальным идентификатором и сохраняет его в Hive Box.
+/
+/ Пример использования:
+/ ```dart
+/ saveToFavorites("Hello", "Привет");
+/ ```
+/ В этом примере текст "Hello" будет сохранен как оригинальный текст,
+/ а "Привет" - как его перевод в избранные.
+/ Если такой перевод уже существует в избранных, он не будет повторно сохранен.
+*/
 void saveToFavorites(String originalText, String translatedText) async {
   final box = Hive.box<Favorites>('Favorites');
 
-  // Check if the entry with the same originalText and translatedText exists
+  // Проверяем, существует ли запись с такими же originalText и translatedText
   final existingFavorite = box.values.firstWhere(
     (favorite) =>
         favorite.originalText == originalText &&
         favorite.translatedText == translatedText,
-    orElse: () =>
-        Favorites('', '', ''), // Return an empty instance if not found
+    orElse: () => Favorites(
+        '', '', ''), // Возвращаем пустой экземпляр, если запись не найдена
   );
 
-  // If the entry does not exist, add a new one to favorites
+  // Если запись не существует, добавляем новую запись в избранные
   if (existingFavorite.id.isEmpty) {
     final id = DateTime.now().millisecondsSinceEpoch.toString();
     final favorite = Favorites(id, originalText, translatedText);
